0606f593922565627acecb1687efa882ba3251b1
Nguyen Dinh Long <nglongd@gmail.com>
2024-08-27 13:50:28 +0700

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,33 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  return res.send(JSON.stringify(books[isbn]));
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const isbnList = Object.keys(books);
+  let booksFilteredByAuthor = {
+    "booksbyauthor": []
+  };
+
+  
+  for (let i = 0; i < isbnList.length; i++){
+    const isnb = isbnList[i];
+    const book = books[isnb];
+    if(book.author === author){
+      const newBook = {
+        "isbn" : isnb,
+        "title" : book.title,
+        "review" : book.reviews
+      }
+      booksFilteredByAuthor["booksbyauthor"].push(newBook);
+    }
+  }
+
+  return res.send(booksFilteredByAuthor);
 });
 
 // Get all books based on title
